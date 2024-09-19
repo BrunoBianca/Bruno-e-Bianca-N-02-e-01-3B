2c3c0800dd81c0828049369b4cef1a039132cc49
BrunoBianca <166662756+BrunoBianca@users.noreply.github.com>
2024-09-19 09:07:20 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const Botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let j = 0; j < Botoes.length; j++){
+        botoes[j].onclick = function(){
+            
+        }
+
+
+}
