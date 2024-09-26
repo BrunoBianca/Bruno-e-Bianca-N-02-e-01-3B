95b3ce3405c2c6bbdfcdaab1d63ceec71ed55293
BrunoBianca <166662756+BrunoBianca@users.noreply.github.com>
2024-09-26 09:01:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let j = 0; j < Botoes.length; j++){
         botoes[j].onclick = function(){
-            
+            for(let i = 0 ; i < botoes.length; i++){
+                botoes[i].classList.remove("")
+            }
         }
-
-
 }
+
+const
