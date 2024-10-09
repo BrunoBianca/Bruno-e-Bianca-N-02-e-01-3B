5e26d5a66cc99ed1256e613849bb7f8b45dff2bc
BrunoBianca <166662756+BrunoBianca@users.noreply.github.com>
2024-10-09 18:31:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let j = 0; j < Botoes.length; j++){
         botoes[j].onclick = function(){
+
             for(let i = 0 ; i < botoes.length; i++){
-                botoes[i].classList.remove("")
+                botoes[j].classList.remove("ativo")
+                botoes[j].classList.remove("ativo")
             }
+
+            botoes[i].classlist.add("ativo");
+            botoes[i].classlist.add("ativo");
         }
 }
 
-const
+const contadores = document.querrySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-09-15T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-09T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-09-11T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
